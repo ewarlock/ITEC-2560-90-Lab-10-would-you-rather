42bc4dfc7e5ae75679041d9e4097b6012f9c3fc1
Edwin <edwin.r.warlock@gmail.com>
2021-04-02 22:04:35 -0500

    initial from videos

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <!--template can only have one child - use div-->
+    <h1>Would You Rather?</h1>
+
+    <!--include components by referencing them in here, by name-->
+    <!--converts uppercase into format with dashes, useful-->
+    <!--but using dash format is optional... but vue prefers it to differentiate from HTML-->
+    <would-you-rather 
+    v-bind:question="wyrQuestion"
+    v-bind:answer1="wyrAnswer1"
+    v-bind:answer2="wyrAnswer2"
+    v-on:answer-changed="answerChanged"
+    ></would-you-rather>
+
+    <h3>{{ userSelectionMessage }}</h3>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WouldYouRather from './components/WouldYouRather.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WouldYouRather
+  },
+  data() {
+    return {
+      wyrQuestion: "Would you rather have a magic carpet that flies or a see-through submarine?",
+      wyrAnswer1: "Magic Carpet",
+      wyrAnswer2: "See-Through Submarine",
+      userSelectionMessage: ""
+    }
+  },
+  methods: {
+    answerChanged(choice) {
+      this.userSelectionMessage = `Thanks! You chose ${choice}`
+    }
   }
 }
 </script>
 
 <style>
+body {
+  background: seagreen;
+  color: white;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/WouldYouRather.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="wyr">
+
+    <h2>Please make your choice:</h2>
+
+    <h3>{{ question }}</h3>
+
+    <input type="radio" 
+    v-model="choice" 
+    v-bind:value="answer1"
+    v-on:change="choiceMade">
+    <label>{{ answer1 }}</label>
+
+    <input type="radio" 
+    v-model="choice" 
+    v-bind:value="answer2"
+    v-on:change="choiceMade">
+    <label>{{ answer2 }}</label>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WouldYouRather',
+  props: { //prop provided by parent, data is for components
+  //fill in data types for props
+    question: String,
+    answer1: String,
+    answer2: String
+  },
+  data() {
+    return {
+      choice: ""
+    }
+  },
+  methods: {
+    choiceMade() {
+      //emits an event, good to use a name with a - to avoid JS conflicts
+      this.$emit('answer-changed', this.choice)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+.wyr {
+  border: 10px green solid;
+  border-radius: 20px;
+  padding: 15px;
+  background: darkslategray;
+}
+</style>
